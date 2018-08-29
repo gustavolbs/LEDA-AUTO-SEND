5acc09c45dfe9f1d3a961a7312be7f4c83f07947
gustavolbs <gusatvo.santos@ccc.ufcg.edu.br>
2018-08-29 14:43:37 -0300

    Commit by gustavolbs at quarta 08/29/18 2018 14:43:36

---
+++ LEDA.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+read -p "Roteiro: " roteiro
+
+matricula='117210400'
+
+wget -O "$roteiro.zip" --post-data="id=$roteiro&matricula=$matricula" http://150.165.85.29:81/download
+unzip  "$roteiro.zip" -d "$roteiro"
+rm -rf "$roteiro.zip"
+
+sed -i "s/INSIRA SEU NUMERO DE MATRICULA/$matricula/g;s/RRX-0X/$roteiro/g" "$roteiro"/pom.xml 
+
+mvn package -Dmaven.test.skip=true
+
+exit 0
