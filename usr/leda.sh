f94a63ea798617b81b01064e6c62990c4e12914d
Gustavo Santos <38411510+gustavolbs@users.noreply.github.com>
2018-08-30 14:33:29 -0300

    Update leda.sh

--- usr/leda.sh
+++ usr/leda.sh
@@ -9,7 +9,7 @@ unzip  "$roteiro.zip" -d "$roteiro"
 rm -rf "$roteiro.zip"
 
 sed -i "s/INSIRA SEU NUMERO DE MATRICULA/$matricula/g;s/RRX-0X/$roteiro/g" "$roteiro"/pom.xml 
-
+cd $roteiro
 mvn package -Dmaven.test.skip=true
 
 exit 0
