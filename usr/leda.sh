c7770aedf0e2b1fa28349b3526a6f93b2556b099
Gustavo Santos <38411510+gustavolbs@users.noreply.github.com>
2018-09-06 17:59:25 -0300

    Update leda.sh

--- usr/leda.sh
+++ usr/leda.sh
@@ -8,7 +8,7 @@ wget -O "$roteiro.zip" --post-data="id=$roteiro&matricula=$matricula" http://150
 unzip  "$roteiro.zip" -d "$roteiro"
 rm -rf "$roteiro.zip"
 
-sed -i "s/INSIRA SEU NUMERO DE MATRICULA/$matricula/g;s/RRX-0X/$roteiro/g" "$roteiro"/pom.xml 
+sed -i "s/INSIRA SEU NUMERO DE MATRICULA/$matricula/g;s/R0X-0X/$roteiro/g" "$roteiro"/pom.xml 
 cd $roteiro
 mvn install -DskipTests
 exit 0
